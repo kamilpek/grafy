34dc8f8fb47c6001321a5bd02f334e78929f7b85
majster <majster@HP-CQ57.home>
2015-03-19 18:59:28 +0100

    poprawki

--- prog2.c
+++ prog2.c
@@ -1,7 +1,7 @@
 /*
 * Algorytmiczna Teroia Grafow.
 * Implementacja grafu w komputerze.
-* (C) Kamil Pek 2015
+* (C)2015 Kamil Pek
 * http://github.com/kamilpek/grafy 
 */
 
@@ -17,7 +17,7 @@ int main(){
 	int v, e;								// liczba wierzchołków i krawędzi
 	int i, j, x, y;							// operatory pętli for
 	int start = 0, stop = 0, status = 0;	// zmienne z duzego fora
-	int max = 0, degree = 0, pa = 0;		// zmienne z malego fora
+	int max = 0, degree = 0;				// zmienne z malego fora
 	
 	printf("Proszę podać liczbę wierzchołków.\n");
 	scanf("%d", &v);
@@ -31,40 +31,35 @@ int main(){
 	for (i=0; i<v; i++) {
 		for (j=0; j<v; j++) {
 			graf[i][j] = 0; } }
-				
-	 for(x=0; x<e; x++){
-     do{
-     			status=1;
-        		printf("Prosze wprowadzic pierwszy wierzcholek krawedzi %d: \t", x+1);
-             	scanf("%d",&start);
-              	printf("Prosze wprowadzic drugi wierzcholek krawedzi %d: \t", x+1);
-               	scanf("%d",&stop); 
-               	
-            
+
+	// wczytywanie grafu	
+	for(x=0; x<e; x++){				// poczatek duzego fora
+		do{
+			status= 1;
+			printf("Prosze wprowadzic pierwszy wierzcholek krawedzi %d: \t", x+1);
+			scanf("%d",&start);
+			printf("Prosze wprowadzic drugi wierzcholek krawedzi %d: \t", x+1);
+			scanf("%d",&stop); 
+             
             if (start == stop) { 
                	printf("Error!! Poczatkowy i koncowy wierzcholek nie moga byc takie same.\n");
-            	status=0; 
-            } 
+            	status=0;	}
                	
             if (start < 1 || stop < 1){ 
                	printf("Error!! Indeks Wierzcholkow nie moze byc mniejszy od 1.\n");
-               	status=0;
-               	}
+               	status=0;	}
 
             if (start > v || stop > v){
                	printf("Error!! Wierzcholek nie moze byc wiekszy od liczby: (%d)!\n", v);
-               	status=0;
-                } 
-        }
-        
-    while (!status);
+               	status=0;	}
+        }        
+		while (!status);
         
 	if (graf[start-1][stop-1]==1){
 		printf("Podany Graf nie moze byc multigrafem.\n");
-	return main();
-	}
+	return main();	}
 	
-	// wczytywanie do tablicy
+	// wczytywanie do macierzy dwuwymiarowej 'graf'
 	graf[start-1][stop-1]=1;
 	graf[stop-1][start-1]=1; 
 	
@@ -75,10 +70,8 @@ int main(){
 	// stopien wierzcholka
 	for(x=0; x<v; x++){    
 		for(y=0; y<=v-1; y++){       		   
-			degree += graf[x][y];
-			if (degree%2 == 0){
-                  pa+=1;
-                } }                         
+			degree += graf[x][y]; }
+		                        
 		printf("\t Wierzcholek o numerze %d ma stopien %d. \n", x+1, degree);
 		if ( degree > max ){ 
 			max = degree;
@@ -90,5 +83,4 @@ int main(){
 	printf("\t Stopien grafu wynosi: %d\n", max);	// Stopien grafu
 	
 	return 0;
-	
 }
